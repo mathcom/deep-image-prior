9aecf0ddc9d1cc8d51840e703f4934c84bcf62fd
mathcom <mathcombio@yonsei.ac.kr>
2023-04-04 17:20:40 +0900

    Fix: suppress a warning message by adding 'align_corners=False' in nn.Unsample

--- models/skip.py
+++ models/skip.py
@@ -78,7 +78,7 @@ def skip(
             deeper.add(deeper_main)
             k = num_channels_up[i + 1]
 
-        deeper.add(nn.Upsample(scale_factor=2, mode=upsample_mode[i]))
+        deeper.add(nn.Upsample(scale_factor=2, mode=upsample_mode[i], align_corners=False))
 
         model_tmp.add(conv(num_channels_skip[i] + k, num_channels_up[i], filter_size_up[i], 1, bias=need_bias, pad=pad))
         model_tmp.add(bn(num_channels_up[i]))
